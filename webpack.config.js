5b8458e649a44eb067a7d5b8e0aab1cd65b32e62
botezatumihaicatalin <botezatu.mihaicatalin@gmail.com>
2017-04-01 21:35:25 +0300

    First commit !

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+var path = require('path')
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var buildFolder   = path.join(__dirname, 'build')
+
+var entryFilepath = path.join(__dirname, 'src', 'Main.js6')
+var bootstrapFilePath = path.join(__dirname, 'bootstrap', 'Bootstrap.js6')
+
+module.exports = {
+  entry:  {
+    main: entryFilepath,
+    bootstrap: bootstrapFilePath
+  },
+  output: {
+    path:     buildFolder,
+    filename: '[name].js'
+  },
+  resolve: {
+    extensions: [ '', '.js', '.js6' ]
+  },
+  target: 'electron',
+  node: {
+    __dirname: false,
+    __filename: false
+  },
+  module: {
+    loaders: [
+      {
+        test:   /\.css$/,
+        loader: 'style!css'
+      },
+      {
+        test:    /\.js6$/,
+        exclude: /node_modules/,
+        loader:  'babel-loader'
+      },
+      {
+        test:   /\.json$/,
+        loader: 'json-loader'
+      },
+      {
+        test: /\.node$/,
+        loader: 'node-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.ejs',
+      inject: 'body',
+      title: 'Webpage'
+    })
+  ]
+}
