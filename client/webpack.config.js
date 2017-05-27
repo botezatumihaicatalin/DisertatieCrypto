b5065b9b862d06338bae715b2d0daa80b4617afc
botezatumihaicatalin <botezatu.mihaicatalin@gmail.com>
2017-05-27 22:45:31 +0300

    Making the app more materialized.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,6 @@
 var path = require('path')
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 var buildFolder   = path.join(__dirname, 'build')
 
@@ -16,7 +17,7 @@ module.exports = {
     filename: '[name].js'
   },
   resolve: {
-    extensions: [ '', '.js', '.js6' ]
+    extensions: [ '.js', '.js6' ]
   },
   resolveLoader: {
     alias: {
@@ -32,7 +33,7 @@ module.exports = {
     loaders: [
       {
         test:   /\.css$/,
-        loader: 'style!css'
+        loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' })
       },
       {
         test:    /\.js6$/,
@@ -46,10 +47,19 @@ module.exports = {
       {
         test: /\.node$/,
         loader: 'node-loader'
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader:"url-loader?limit=10000&mimetype=application/font-woff"
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader"
       }
     ]
   },
   plugins: [
+    new ExtractTextPlugin("[name].css"),
     new HtmlWebpackPlugin({
       template: 'index.ejs',
       inject: 'body',
